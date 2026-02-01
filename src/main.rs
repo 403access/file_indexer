33eb70bb18c288d96c72344efa68d8da72b42b6f
Oliver Molnar <oliver.molnar@420cloud.io>
2026-02-01 20:59:37 +0100

    feat: api server - use tokio with axum

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use std::{env, io};
+use std::env;
 
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+use file_indexer::api::{create_user, root};
 use file_indexer::modules::{
-    arguments::check_arguments::check_arguments, commands::commands_loop::commands_loop,
+    arguments::check_arguments::check_arguments,
+    // commands::commands_loop::commands_loop,
     environment::check_vars::check_vars,
 };
 use file_indexer::states::app_state;
 
-fn main() -> io::Result<()> {
+// fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
 
     check_arguments(&args).unwrap();
@@ -19,5 +28,19 @@ fn main() -> io::Result<()> {
     // Initialize application state
     app_state::init(path, database_url);
 
-    commands_loop()
+    // commands_loop()
+
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
