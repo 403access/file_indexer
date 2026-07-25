2d30b4ca54c9289d99d86c88a92f752ded342725
Oliver Molnar <oliver.molnar@420cloud.io>
2026-07-25 20:42:53 +0200

    fix: use fallback_service instead of nest_service for static files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-use std::env;
-
 use axum::Router;
 use tower_http::services::ServeDir;
 
 use file_indexer::api::create_router;
-use file_indexer::modules::{
-    arguments::check_arguments::check_arguments,
-    environment::check_vars::check_vars,
-};
+use file_indexer::modules::environment::check_vars::check_vars;
 use file_indexer::states::app_state::{self, AppState};
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = env::args().collect();
-    check_arguments(&args).unwrap();
-
     check_vars();
     let path = file_indexer::modules::environment::env_vars::get_cwd();
     let database_url = file_indexer::modules::environment::env_vars::get_database_url();
@@ -31,7 +23,7 @@ async fn main() {
     let api_router = create_router(state);
 
     let app = Router::new()
-        .nest_service("/", ServeDir::new("static"))
+        .fallback_service(ServeDir::new("static"))
         .merge(api_router);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
